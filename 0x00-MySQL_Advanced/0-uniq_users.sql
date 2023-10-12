304b3d4516a72aad6f10302b281a8b381eafadf3
atck3r <atck3r@localdomain>
2023-10-12 22:17:21 +0300

    Fixed

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -2,7 +2,7 @@
 -- SQL script that creates a table users
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
-    id INT,
-    email VARCHAR(255),
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255)
 );
