734b0d71b9ebb6a872f7a4ce8a700cae31c4e4b0
atck3r <atck3r@localdomain>
2023-10-12 22:19:59 +0300

    Fixed

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,5 @@
 -- 3. Old school band
-SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
+SELECT band_name, (IFNULL(split, '2020') - IFNULL(formed, 0)) AS lifespan
     FROM metal_bands
-    WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
+    WHERE style LIKE '%Glam rock%'
     ORDER BY lifespan DESC;
