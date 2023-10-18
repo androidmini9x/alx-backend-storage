968db7afe7f01ce5e22222505f9d07c2b983bd36
atck3r <atck3r@localdomain>
2023-10-18 22:06:40 +0300

    FS

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,36 +1,37 @@
 #!/usr/bin/env python3
-"""
-Caching request module
-"""
+"""Implementing an expiring web cache and tracker"""
+
+from functools import wraps
 import redis
 import requests
-from functools import wraps
 from typing import Callable
 
+r = redis.Redis()
 
-def track_get_page(fn: Callable) -> Callable:
-    """ Decorator for get_page
-    """
-    @wraps(fn)
-    def wrapper(url: str) -> str:
-        """ Wrapper that:
-            - check whether a url's data is cached
-            - tracks how many times get_page is called
-        """
-        client = redis.Redis()
-        client.incr(f'count:{url}')
-        cached_page = client.get(f'{url}')
-        if cached_page:
-            return cached_page.decode('utf-8')
-        response = fn(url)
-        client.set(f'{url}', response, 10)
-        return response
+
+def count_requests(method: Callable) -> Callable:
+    """ Decortator for counting how many times a request
+    has been made """
+
+    @wraps(method)
+    def wrapper(url):
+        """ Wrapper for decorator functionality """
+        r.incr(f"count:{url}")
+        cached_html = r.get(f"cached:{url}")
+        if cached_html:
+            return cached_html.decode('utf-8')
+
+        html = method(url)
+        r.setex(f"cached:{url}", 10, html)
+        return html
+
     return wrapper
 
 
-@track_get_page
+@count_requests
 def get_page(url: str) -> str:
-    """ Makes a http request to a given endpoint
+    """Uses the requests module to obtain the HTML
+    content of a particular URL and returns it.
     """
-    response = requests.get(url)
-    return response.text
+    req = requests.get(url)
+    return req.text
