dff38eb8db831cde90dc04445aba6be6cdcc63ee
atck3r <atck3r@localdomain>
2023-10-18 18:29:49 +0300

    Init

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+'''
+0. Writing strings to Redis
+'''
+import redis
+import uuid
+from typing import Union
+
+
+class Cache:
+    '''Implementation of redis class'''
+    def __init__(self) -> None:
+        '''Init redis cache'''
+        self._redis = redis.Redis()
+        self._redis.flushall()
+
+    def store(self, data: Union[str, int, bytes, float]) -> str:
+        '''Store data by generated key'''
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
