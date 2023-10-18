7edf93854277b71304cef52ad99f247b0b421e71
atck3r <atck3r@localdomain>
2023-10-18 21:41:33 +0300

    Fixed

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -15,16 +15,15 @@ def cache_data(method: Callable) -> Callable:
     Count how many times methods called
     '''
     @wraps(method)
-    def wrapper(url) -> str:
+    def wrapper(*args, **kwargs) -> str:
         '''Increment the called method'''
-        key_count = 'count:'.format(url)
-        key_result = 'result:'.format(url)
+        key_count = 'count:'.format(args[0])
+        key_result = 'result:'.format(args[0])
         cache.incr(key_count)
         if cache.exists(key_result) == 1:
             return cache.get(key_result).decode('utf-8')
         # If not exists reset cache count & result
-        respone = method(url)
-        cache.set(key_count, 0)
+        respone = method(*args, **kwargs)
         cache.setex(key_result, 10, respone)
         return respone
     return wrapper
