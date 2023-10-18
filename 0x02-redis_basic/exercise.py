a74757d93aab6d90ced79f63b702b2e0648c7bbd
atck3r <atck3r@localdomain>
2023-10-18 18:55:43 +0300

    Fixed 2

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -11,10 +11,10 @@ class Cache:
 
     def __init__(self) -> None:
         self._redis = redis.Redis()
-        self._redis.flushdb()
+        self._redis.flushdb(True)
 
-    def store(self, data: Union[str, int, bytes, float]) -> str:
-        '''Store data by generated key'''
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        '''Store data to Redis using the generated key'''
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
