9a2ce98316b5a94aaabe6c85cb4a2878e34e36a7
atck3r <atck3r@localdomain>
2023-10-18 21:47:19 +0300

    Te2

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,38 +1,33 @@
 #!/usr/bin/env python3
-'''A module with tools for request caching and tracking.
-'''
+"""5. Implementing an expiring web cache and tracker"""
+
+from requests import get
 import redis
-import requests
 from functools import wraps
 from typing import Callable
 
 
-redis_store = redis.Redis()
-'''The module-level Redis instance.
-'''
+def track_page_count(method: Callable) -> Callable:
+    """Tracks how many times a particular
+    URL was accessed"""
 
-
-def data_cacher(method: Callable) -> Callable:
-    '''Caches the output of fetched data.
-    '''
     @wraps(method)
-    def invoker(url) -> str:
-        '''The wrapper function for caching the output.
-        '''
-        redis_store.incr(f'count:{url}')
-        result = redis_store.get(f'result:{url}')
-        if result:
-            return result.decode('utf-8')
-        result = method(url)
-        redis_store.set(f'count:{url}', 0)
-        redis_store.setex(f'result:{url}', 10, result)
-        return result
-    return invoker
+    def wrapper(*args, **kwargs):
+        """Inner Wrapper Function"""
+        cache = redis.Redis()
+        url = args[0]
+        key = 'count:' + url
+        cache.incr(key)
+        page = cache.get(url)
+        if page is not None:
+            return page.decode('utf-8')
+        page = method(*args, **kwargs)
+        cache.setex(key, 10, page)
+        return page
+    return wrapper
 
 
-@data_cacher
 def get_page(url: str) -> str:
-    '''Returns the content of a URL after caching the request's response,
-    and tracking the request.
-    '''
-    return requests.get(url).text
+    """Obtain the HTML content of a particular
+    URL and returns it."""
+    return get(url).text
